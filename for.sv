35dfda8d0e1eed4473ec6e9ccd59dc78a49b194b
nancydimri <nancydimri@mirafra.com>
2022-09-20 04:16:14 +0530

    Added one file by nancy

---
+++ for.sv
@@ -0,0 +1,19 @@
+// For loop - For loop is use to execute the statements again and agains .When the condition ofr for loop 
+//          will not true , it will get terminate and the user will come out from the loop .
+//  Syntax - // declaring the data type to the variable 
+//              assign the value to the variable 
+//              initial begin 
+//              for ([initialization]; [condition]; [modifier])
+//               begin 
+//               Statements;
+//               end
+//               end// 
+//               
+// Example - 
+//
+module for_code;
+
+        
+
+
+
